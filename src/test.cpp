f0c203350103467041c171cb9dd276e3bbb6eae4
LHEUR <laurent@lheur.com>
2021-03-09 11:01:37 +0100

    Add vector

--- src/test.cpp
+++ src/test.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 auto main() -> int {
 
     std::string s{"chaine de caractère"};
+    std::vector<int> v{1,2,3,4,5}; 
+  
     std::cout << s << std::endl;
-    
+
+    for(auto val : v) {
+        std::cout << val << std::endl;
+    }
+
     return 0;
 } 
